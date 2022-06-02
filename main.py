eb0da24d0b0f38d994aedd986f0f99fdbc60ef9c
Bartosz <bartosz.szarkowski@student.put.poznan.pl>
2022-06-02 21:59:19 +0200

    v1

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import csv
 
-if __name__ == "__main__":
-    pass
+with open('phoneCalls.csv', 'r') as fin:
+    reader = csv.reader(fin, delimiter = ",")
+    headers = next(reader)
+
+    for row in reader:
+        print(row)
+        from_subsr = int([0])
+        if from_subsr not in call_dict_sum:
+
+        calls_dict_sum[from_subsr] += int(row[3])
