be6aa5906cdd53ad716a1001514674593db0307f
Bartosz <bartosz.szarkowski@student.put.poznan.pl>
2022-06-03 20:49:28 +0200

    finish

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
-if __name__ == "__main__":
+import csv
 
-    import csv
+class MenadzerPolaczen:
+  def __init__(self, filename):
+    self.filename = filename
+    self.data_dict = self.read_data()
 
-    with open('phoneCalls.csv', 'r') as fin:
-     reader = csv.reader(fin, delimiter = ",")
-     headers = next(reader)
+  def read_data(self):
+    calls_dict_sum = dict()
+    with open(self.filename, 'r') as fin:
+      reader = csv.reader(fin, delimiter= ",")
+      headers = next(reader)
 
-     for row in reader:
-         print(row)
-         from_subsr = int([0])
-         if from_subsr not in call_dict_sum:
+      for row in reader:
+        from_subsr = int(row[0])
+        if from_subsr not in calls_dict_sum:
+          calls_dict_sum[from_subsr] = 0
+        calls_dict_sum[from_subsr] += 1
+    return calls_dict_sum
 
-         calls_dict_sum[from_subsr] += int(row[3])
+  def pobierz_najczesciej_dzwoniacego(self):
+    return max(self.data_dict.items(), key= lambda x: x[1])
+
+if __name__ == '__main__':
+  print(MenadzerPolaczen(input()).pobierz_najczesciej_dzwoniacego())
